3ea5d211bf94e0488fb3d5cdc981d99124b177f7
gaoyu <2282940822@qq.com>
2018-10-31 16:21:06 +0800

    add a code

---
+++ func_binary.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: func_binary.c
+	> Author:gaoyu 
+	> Mail:
+	> Created Time: 2018年08月15日 星期日 18时56分09秒
+ ************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int func_binary(int x) {
+    return x * x;
+}
---
+++ func_binary.h
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: func_binary.h
+	> Author: gaoyu
+	> Mail: 
+	> Created Time: 2018年08月15日 星期日 18时56分43秒
+ ************************************************************************/
+
+#ifndef _FUNC_BINARY_H
+#define _FUNC_BINARY_H
+
+int func_binary(int);
+
+#endif
