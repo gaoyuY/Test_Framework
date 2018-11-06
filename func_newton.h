d013cd8c27de9c7dd7c7bfdd0f2fae19627d78aa
gaoyu <2282940822@qq.com>
2018-11-06 16:30:30 +0800

    remove code

---
+++ func_newton.h
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: func_newton.h
+	> Author: gaoyu
+	> Mail: 
+	> Created Time: 2018年08月15日 星期三 19时57分43秒
+ ************************************************************************/
+
+#ifndef _FUNC_NEWTON_H
+#define _FUNC_NEWTON_H
+
+double f1(double, double);
+double f1_prime(double);
+
+#endif
